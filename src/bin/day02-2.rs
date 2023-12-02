7c152b789a1cf15ddb698f15cbd6f04976c865ef
Matti Korttila <matti.korttila@kipina.fi>
2023-12-02 21:33:08 +0200

    day 2

---
+++ src/bin/day02-2.rs
@@ -0,0 +1,94 @@
+use regex::Regex;
+use std::fs;
+
+#[derive(Debug)]
+struct Cubes {
+    red: u32,
+    blue: u32,
+    green: u32,
+}
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    sets: Vec<Cubes>,
+}
+
+fn parse_game(line: &str) -> Game {
+    let mut sets: Vec<Cubes> = Vec::new();
+    let re = Regex::new(r"Game (\d+)").unwrap();
+    let id: u32 = match re.captures(line) {
+        Some(x) => x.get(1).map_or(0, |m| m.as_str().parse::<u32>().unwrap()),
+        None => 0,
+    };
+    let raw_sets = line.split(";");
+
+    for raw in raw_sets {
+        // eprintln!("{raw}");
+        let re = Regex::new(r"(\d+) red").unwrap();
+        let red: u32 = match re.captures(raw) {
+            Some(x) => x.get(1).map_or(0, |m| m.as_str().parse::<u32>().unwrap()),
+            None => 0,
+        };
+        let re = Regex::new(r"(\d+) green").unwrap();
+        let green: u32 = match re.captures(raw) {
+            Some(x) => x.get(1).map_or(0, |m| m.as_str().parse::<u32>().unwrap()),
+            None => 0,
+        };
+        let re = Regex::new(r"(\d+) blue").unwrap();
+        let blue: u32 = match re.captures(raw) {
+            Some(x) => x.get(1).map_or(0, |m| m.as_str().parse::<u32>().unwrap()),
+            None => 0,
+        };
+        sets.push(Cubes { red, green, blue });
+    }
+
+    Game { id, sets }
+}
+
+fn solve(data: &str) -> u32 {
+    let games = data.lines().map(parse_game);
+
+    games
+        .map(|game| {
+            // eprintln!("game {:?}", game);
+            let mut red_max = 0;
+            let mut green_max = 0;
+            let mut blue_max = 0;
+
+            for set in &game.sets {
+                if set.red > red_max {
+                    red_max = set.red;
+                }
+                if set.green > green_max {
+                    green_max = set.green;
+                }
+                if set.blue > blue_max {
+                    blue_max = set.blue;
+                }
+            }
+            eprintln!("{} {} {}", red_max, green_max, blue_max);
+            red_max * green_max * blue_max
+        })
+        .sum()
+}
+
+fn main() {
+    println!("Hello, day 2!");
+    let lines = fs::read_to_string("data/day-02.txt").unwrap();
+    println!("the answer is {}", solve(&lines));
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_example_data() {
+        let data = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        assert_eq!(2286, solve(data));
+    }
+}
